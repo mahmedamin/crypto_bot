5cc178cf43fabfd0ec60920b42ef218dec078c9f
Ahmed <ahmed.udhi@gmail.com>
2022-02-28 05:34:09 +0500

    Configuration changes

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
+const {env} = process;
 
-const sequelize = new Sequelize('crypto_bot', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
+const sequelize = new Sequelize(env.DB_DATABASE, env.DB_USERNAME, env.DB_PASSWORD, {
+    dialect: env.DB_DIALECT,
+    host: env.DB_HOST,
+    logging: env.DB_LOGGING,
     timezone: '+05:00'
 });
 
